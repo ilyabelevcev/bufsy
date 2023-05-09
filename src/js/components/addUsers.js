f3ef1aeda71ad87728f575c5bab8cefa365885a6
Ilya <ilyabelevcev00@gmail.com>
2023-05-09 20:00:36 +0300

    finish

--- src/js/components/addUsers.js
+++ src/js/components/addUsers.js
@@ -7,24 +7,45 @@ export const addUsers = async (page, limit) => {
 
     for (let user of data) {
         const li = document.createElement("li");
-        const spanName = document.createElement("span");
-        const spanEmail = document.createElement("span");
-        const spanPrize = document.createElement("span");
+        li.className = "col-xs-12 col-sm-12 col-md-12 col-lg-12 winners__tbody-user";
 
-        li.className = "col-lg-12 winners__tbody-user";
-        spanName.className = "col-lg-3";
-        spanEmail.className = "col-lg-3";
-        spanPrize.className = "col-lg-6";
+        const divHead = document.createElement("div");
+        const spanNameHead = document.createElement("span");
+        const spanEmailHead = document.createElement("span");
+        const spanPrizeHead = document.createElement("span");
 
-        spanName.innerHTML = user.id;
-        spanEmail.innerHTML = user.title;
-        spanPrize.innerHTML = user.body;
+        divHead.className = "col-xs-4"
+        spanNameHead.className = "col-xs-12";
+        spanEmailHead.className = "col-xs-12";
+        spanPrizeHead.className = "col-xs-12";
+
+        spanNameHead.innerHTML = 'Имя';
+        spanEmailHead.innerHTML = 'Email';
+        spanPrizeHead.innerHTML = 'Приз';
+
+        const divBody = document.createElement("div");
+        const spanNameBody = document.createElement("span");
+        const spanEmailBody = document.createElement("span");
+        const spanPrizeBody = document.createElement("span");
+
+        divBody.className = "col-xs-8 col-sm-12 col-md-12 col-lg-12"
+        spanNameBody.className = "col-xs-12 col-sm-2 col-md-2 col-lg-2";
+        spanEmailBody.className = "col-xs-12 col-sm-4 col-md-3 col-lg-3";
+        spanPrizeBody.className = "col-xs-12 col-sm-6 col-md-7 col-lg-7";
+
+
+        spanNameBody.innerHTML = user.id;
+        spanEmailBody.innerHTML = user.title.split(' ')[0];
+        spanPrizeBody.innerHTML = user.body;
+
 
         vars.usersList.append(li);
-        li.append(spanName, spanEmail, spanPrize);
+        li.append(divHead, divBody);
+        divHead.append(spanNameHead, spanEmailHead, spanPrizeHead)
+        divBody.append(spanNameBody, spanEmailBody, spanPrizeBody);
     }
 
-    vars.usersList.style.height = null;
+    vars.usersList.style.height = 'auto';
     vars.usersList.classList.add("animation");
 };
 addUsers();
